65ab267d13c03399f93bf99ecf9f932a2e9ce281
Andrew Oldroyd <andrew.oldroyd7@gmail.com>
2023-09-24 23:41:41 -0600

    functions part mostly done

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,10 @@
 // Ex.:
 //   divide(10, 2);
 //   => 5
-function divide(x, y) {}
+function divide(x, y) {
+  return (x / y)
+}
+console.log(divide(10, 2))
 
 // Return the average of x and y.
 // Here, the average of x and y is the *mean* of x and y. In other words, it's
@@ -10,7 +13,11 @@ function divide(x, y) {}
 // Ex.:
 //   average(10, 2);
 //   => 6
-function average(x, y) {}
+function average(x, y) {
+  return ((x+y)/2)
+}
+
+console.log(average(5, 7))
 
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
 // In other words, return true if the aboslute value of x - y is less than 0.001.
@@ -23,26 +30,45 @@ function approximatelyEqual(x, y) {}
 // Ex.:
 //   fullName('John', 'Doe');
 //   => 'John Doe'
-function fullName(firstName, lastName) {}
+function fullName(firstName, lastName) {
+  return (firstName + " " + lastName)
+}
+//need all caps? .toUpperCase for all caps.
+console.log(fullName("Andrew", "Oldroyd"))
 
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
 // Ex.:
 //   generateSentence('Kay', 'coffee', 'the local cafe');
 //   => 'Kay was drinking coffee at the local cafe.'
-function generateSentence(person, beverage, location) {}
+function generateSentence(person, beverage, location) {
+  return (`${person} was drinking ${beverage} at ${location}.`)
+}
+
+console.log(generateSentence("Andrew", "egg nog", "the north pole"))
 
 // Return the given string with all vowels replced with '*' characters.
 // Ex.:
 //   censorVowels('javascript');
 //   => 'j*v*scr*pt'
-function censorVowels(string) {}
+function censorVowels(string) {
+
+  return string.replace(["a", "e", "i", "o", "u"], "*")
+
+}
+
+console.log(censorVowels("javascript"))
 
 // Return the given string in sticky case.
 // Ex.:
 //   stickyCase('hello world');
 //   => 'hElLo wOrLd'
-function stickyCase(string) {}
+function stickyCase(string) {
+
+  return string.insertFunctionForStickyCase()
+}
+
+console.log(stickyCase("hello world"))
 
 // Return the given string in leetspeak. Leetspeak is a modified version of
 // English where characters are replaced by numbers or symbols. For this
@@ -56,7 +82,11 @@ function stickyCase(string) {}
 // Ex.:
 //   leetspeak('javascript');
 //   => 'j4v45cr1p7'
-function leetspeak(string) {}
+function leetspeak(string) {
+
+  
+
+}
 
 export {
   approximatelyEqual,
